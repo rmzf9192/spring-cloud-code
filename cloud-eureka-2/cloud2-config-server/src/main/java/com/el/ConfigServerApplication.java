170f8decf8f66f880ed4cae22ab40c4312d3796c
roman.zhang <1187455023@qq.com>
2020-02-14 15:41:56 +0800

    eureka 在线动态扩容

---
+++ cloud-eureka-2/cloud2-config-server/src/main/java/com/el/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.el;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author roman zhangfei
+ * @Date 2020/2/14
+ * @Version V1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
---
+++ cloud-eureka-2/cloud2-eureka-server/src/main/java/com/el/contorller/QueryController.java
@@ -0,0 +1,24 @@
+package com.el.contorller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author roman zhangfei
+ * @Date 2020/2/14
+ * @Version V1.0
+ */
+@RestController
+@RequestMapping("/query")
+public class QueryController {
+    @Autowired
+    EurekaClientConfigBean eurekaClientConfigBean;
+
+    @GetMapping("/eureka-server")
+    public Object getEurekaServerUrl(){
+        return eurekaClientConfigBean.getServiceUrl();
+    }
+}
