a32e1ac32f39551d13f8bc800dd0040f1acf18b6
roman.zhang <1187455023@qq.com>
2020-02-22 13:06:26 +0800

    config整合mysql案例

---
+++ cloud11-3-config/cloud11-3-config-client-db/src/main/java/com/el/cloud/DbConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.el.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author roman zhangfei
+ * @Date 2020/2/22
+ * @Version V1.0
+ */
+@SpringBootApplication
+public class DbConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbConfigClientApplication.class, args);
+    }
+}
